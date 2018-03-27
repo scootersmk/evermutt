351c5d5ad5ef30c4b2ac6e162d0ac5c65de419a3
Scott Koch <scottkoch@gmail.com>
2018-03-26 20:59:13 -0400

    basic cache code, fixed some lint errors

--- evermutt.py
+++ evermutt.py
@@ -10,6 +10,8 @@ import time
 import argparse
 import local
 import xml.etree.ElementTree as ET
+import os
+import sys
 
 
 KEYS_ENTER = (curses.KEY_ENTER, ord('\n'), ord('\r'))
@@ -76,7 +78,10 @@ class NoteList:
     status_line = "Title: %s" % (note.title)
     status_line_length = x - 1
     status_line_fmt = "%%-%ds" % status_line_length
-    note_screen.addstr(status_y, status_x, status_line_fmt % status_line, curses.color_pair(1))
+    note_screen.addstr(status_y,
+                       status_x,
+                       status_line_fmt % status_line,
+                       curses.color_pair(1))
     note_screen.addstr(1, 0, "Title: %s" % note.title)
     created_date_str = convert_epoch_to_date(note.created, False)
     updated_date_str = convert_epoch_to_date(note.updated, False)
@@ -127,11 +132,90 @@ def login(args):
   session['userStore'] = session['client'].get_user_store()
   session['noteStore'] = session['client'].get_note_store()
   session['defaultNotebook'] = session['noteStore'].getDefaultNotebook()
-  session['noteMetadata'] = get_notes_metadata(session['noteStore'], session['defaultNotebook'])
+  session['noteMetadata'] = get_notes_metadata(session['noteStore'],
+                                               session['defaultNotebook'])
   session['noteCount'] = len(session['noteMetadata'])
   if args.verbose:
     print "NoteCount = %d" % session['noteCount']
   return session
+
+def cache(session, offline=False):
+  cached_session = {}
+  create_cache_dir = False
+  new_cache = False
+
+  #Verify/Create local storage
+  home_env = 'HOME'
+  evermutt_dirname = ".evermutt"
+  try:
+    homedir = os.environ[home_env]
+  except KeyError:
+    print "Error, no such environment variable: %s" % home_env
+    sys.exit(1)
+  evermutt_dir = "%s%s%s" % (homedir, os.sep, evermutt_dirname)
+
+  try:
+    os.stat(homedir)
+  except OSError:
+    print "Error, HOME directory(%s) does not exist \n \
+          Please set HOME to a writeable directory or \
+          disable cacheing with --no-cache option"
+    sys.exit(1)
+
+  try:
+    os.stat(evermutt_dir)
+  except OSError:
+    create_cache_dir = True
+
+  if create_cache_dir:
+    try:
+      os.mkdir(evermutt_dir, 0o700)
+      new_cache = True
+    except OSError:
+      print "Error, cache directory creation failed"
+      sys.exit(1)
+
+  #FIXME: Read in local cache
+  #if not new_cache:
+  #  print "Read cache"
+
+  #Get note content
+  if not offline:
+    for idx, note in enumerate(session['noteMetadata']):
+      content_fname = "%s%s%s.xml" % (evermutt_dir, os.sep, note.guid)
+      #print "Cacheing note %s in %s" % (note.guid, content_fname)
+      content = get_note_content(idx, session, True)
+      #print content
+      fdesc = open(content_fname, "w")
+      fdesc.write(content)
+      fdesc.close()
+      #print "Created file %s" % fname
+      metadata_fname = "%s%s%s.metadata.xml" % (evermutt_dir, os.sep, note.guid)
+      #print "Cacheing note %s in %s" % (note.guid, content_fname)
+      #FIXME: Add cache attributes to root element: guid, version/timestamp, etc
+      note_cache_xml = ET.Element('note')
+      tags_xml = ET.SubElement(note_cache_xml, 'tags')
+      tags = get_note_tags(idx, session)
+      for t in tags:
+        attribs = {}
+        attribs['text'] = t
+        tag_xml = ET.SubElement(tags_xml, 'tag', attribs)
+      #print content
+      fdesc = open(metadata_fname, "w")
+      metadata_content = ET.tostring(note_cache_xml)
+      fdesc.write(metadata_content)
+      fdesc.close()
+      #print "Created file %s" % fname
+
+  #FIXME: Download resources
+
+  #FIXME: Handle conflicts
+  #if not new_cache:
+  #  print "Merge cache with server data"
+
+  #Write cache to disk
+  return cached_session
+
 
 def update_status(scr, session):
   notebook = session['defaultNotebook'].name
@@ -142,7 +226,10 @@ def update_status(scr, session):
   status_line = "Notebook: %s [Notes: %d]" % (notebook, noteCount)
   status_line_length = x - 1
   status_line_fmt = "%%-%ds" % status_line_length
-  scr.addstr(status_y, status_x, status_line_fmt % status_line, curses.color_pair(1))
+  scr.addstr(status_y,
+             status_x,
+             status_line_fmt % status_line,
+             curses.color_pair(1))
   scr.refresh()
 
 def convert_epoch_to_date(epoch, short=True):
@@ -153,7 +240,6 @@ def convert_epoch_to_date(epoch, short=True):
     return time.strftime("%a, %d %b %Y %H:%M:%S %Z", time.localtime(epoch))
 
 def get_notes_metadata(ns, nb):
-  #http://dev.evernote.com/doc/reference/NoteStore.html#Fn_NoteStore_findNotesMetadata
   notefilter = NotesStore.NoteFilter()
   notefilter.notebookGuid = nb.guid
   resultspec = NotesStore.NotesMetadataResultSpec()
@@ -165,7 +251,9 @@ def create_note(session, text):
   ns = session['noteStore']
   note = Types.Note()
   note.title = text
-  note.content = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
+  note.content = '<?xml version="1.0" encoding="UTF-8"?>\
+                 <!DOCTYPE en-note SYSTEM \
+                 "http://xml.evernote.com/pub/enml2.dtd">'
   note.content += '<en-note></en-note>'
   note = ns.createNote(note)
 
@@ -185,7 +273,7 @@ def gui(stdscr, session):
   nl = NoteList(notes_screen, session)
   note_index = nl.run_loop()
 
-  return
+  return note_index
 
 def parse_note_content(en_xml):
   lines = []
@@ -204,12 +292,15 @@ def parse_note_content(en_xml):
 
   return lines
 
-def get_note_content(index, session):
+def get_note_content(index, session, raw=False):
   ns = session['noteStore']
   notes = session['noteMetadata']
   #FIXME: replace getNote call with getNoteWithResultSpec
   note = ns.getNote(notes[index].guid, True, False, False, False)
-  return parse_note_content(note.content)
+  if raw:
+    return note.content
+  else:
+    return parse_note_content(note.content)
 
 def get_note_tags(index, session):
   ns = session['noteStore']
@@ -220,22 +311,48 @@ def get_note_tags(index, session):
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+  parser.add_argument("-v", 
+                      "--verbose",
+                      help="increase output verbosity",
+                      action="store_true")
   #FIXME: customize create-note option help text to make it clearer how to use
-  parser.add_argument('-c', '--create-note', help="Create a note in the default notebook")
+  parser.add_argument('-c',
+                      '--create-note',
+                      help="Create a note in the default notebook")
+  parser.add_argument('-o',
+                      '--offline',
+                      help="Work within local cache, don't talk to server",
+                      action="store_true")
+  parser.add_argument('--no-cache',
+                      help="Disable creating or using local cache directory",
+                      action="store_true")
+  #FIXME: -F option to specify different cache/config directory
+  #FIXME: -f option to specify different config file
   args = parser.parse_args()
+
+  if args.offline and args.no_cache:
+    print "Invalid options, can't set both --offline and --no-cache"
+    sys.exit(1)
 
   if args.verbose:
     print "verbosity turned on"
 
-  session = login(args)
+  if args.offline:
+    session = None
+  else:
+    session = login(args)
 
   if args.create_note:
     if args.verbose:
       print "Creating new note"
     create_note(session, args.create_note)
   else:
-    curses.wrapper(gui, session)
+    if args.no_cache:
+      curses.wrapper(gui, session)
+    else:
+      cached_session = cache(session)
+      return
+      curses.wrapper(gui, cached_session)
 
 if __name__ == '__main__':
   main()
