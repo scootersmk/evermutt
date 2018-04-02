852efe09287e5b3e9c48626c9e7b3db1d4d8d5dd
Scott Koch <scottkoch@gmail.com>
2018-04-02 19:45:43 -0400

    first tests

---
+++ tests/test_misc.py
@@ -0,0 +1,20 @@
+import unittest
+
+import sys
+sys.path.insert(0,'..')
+from misc import *
+
+class TestMisc(unittest.TestCase):
+
+    def test_epoch_to_date_short(self):
+        epoch = 1522705836
+        date_str = convert_epoch_to_date(epoch)
+        assert date_str == 'Apr 02'
+
+    def test_epoch_to_date_long(self):
+        epoch = 1522705836
+        date_str = convert_epoch_to_date(epoch, False)
+        assert date_str == 'Mon, 02 Apr 2018 17:50:36 EDT'
+
+if __name__ == '__main__':
+    unittest.main()
