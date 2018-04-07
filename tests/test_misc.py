b75bc1979386ce3c6f5aecc01538d0b6de2c461d
Scott Koch <scottkoch@gmail.com>
2018-04-07 16:32:53 -0400

    fix epoch conversion tests

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -7,12 +7,12 @@ from misc import *
 class TestMisc(unittest.TestCase):
 
     def test_epoch_to_date_short(self):
-        epoch = 1522705836
+        epoch = 1522705836000
         date_str = convert_epoch_to_date(epoch)
         assert date_str == 'Apr 02'
 
     def test_epoch_to_date_long(self):
-        epoch = 1522705836
+        epoch = 1522705836000
         date_str = convert_epoch_to_date(epoch, False)
         assert date_str == 'Mon, 02 Apr 2018 17:50:36 EDT'
 
